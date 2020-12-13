1ad560a24cfddd05045282fcc12b958123235be7
Huy Vinh Le <HuyVinhLe@msn.com>
2020-12-13 21:36:03 +0000

    demo_2

--- LeHuyVinh/models/clearAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Deleting All Models
-rosservice call gazebo/delete_model final_setup_test
-rosservice call gazebo/delete_model dd_robot
-rosservice call gazebo/delete_model final_setup
-
-rosrun gazebo_ros spawn_model -file ~/catkin_ws/src/hle25_final/models/model.sdf -sdf -x 0 -y 0 -z .1 -model dd_robot
--- LeHuyVinh/models/final.sh
+++
@@ -1,2 +0,0 @@
-rosservice call gazebo/delete_model final_setup
-rosrun gazebo_ros spawn_model -file final.sdf -sdf -model final_setup -y -0.0 -x -0.0 -z 0.0
