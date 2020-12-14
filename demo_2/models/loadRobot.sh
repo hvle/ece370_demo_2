40a2988d0f9486f2ace1ae0e4d30a29cf3989827
Huy Vinh Le <huyvinhle@msn.com>
2020-12-14 06:28:46 +0000

    corrected paths

--- demo_2/models/loadRobot.sh
+++ demo_2/models/loadRobot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rosservice call gazebo/delete_model dd_robot
-rosrun gazebo_ros spawn_model -file ~/catkin_ws/src/demo_2/models/model.sdf -sdf -x $1 -y $2 -z 1 -model dd_robot
+rosrun gazebo_ros spawn_model -file model.sdf -sdf -x $1 -y $2 -z 1 -model dd_robot
--- demo_2/models/spawnBox.sh
+++ demo_2/models/spawnBox.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rosrun gazebo_ros spawn_model -file ~/catkin_ws/src/demo_2/models/box.urdf -urdf -x $1 -y $2 -z 5 -model box$3 &
+rosrun gazebo_ros spawn_model -file box.urdf -urdf -x $1 -y $2 -z 5 -model box$3 &
